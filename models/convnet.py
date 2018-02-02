2ddca3d24f474c9c834b8ca6b4062bcbcccd9897
crcrpar <masaki.kozuki.2014@gmail.com>
2018-02-02 16:06:43 +0900

    update version

--- models/convnet.py
+++ models/convnet.py
@@ -1,44 +1,65 @@
 import math
+
 import chainer
 import chainer.functions as F
 import chainer.links as L
+
 from chainer.initializers import Uniform
-from convbnrelu import ConvBNReLU
+
+from chainercv.links import Conv2DBNActiv
+
+
+def kl_divergence(y, t):
+    entropy = - F.sum(t[t.array.nonzero()] * F.log(t[t.array.nonzero()]))
+    cross_entropy = - F.sum(t * F.log_softmax(y))
+
+    return (cross_entropy - entropy) / y.shape[0]
 
 
 class ConvNet(chainer.Chain):
-    def __init__(self, n_classes):
-        super(ConvNet, self).__init__(
-            conv11=ConvBNReLU(3, 64, 3, pad=1),
-            conv12=ConvBNReLU(64, 64, 3, pad=1),
-            conv21=ConvBNReLU(64, 128, 3, pad=1),
-            conv22=ConvBNReLU(128, 128, 3, pad=1),
-            conv31=ConvBNReLU(128, 256, 3, pad=1),
-            conv32=ConvBNReLU(256, 256, 3, pad=1),
-            conv33=ConvBNReLU(256, 256, 3, pad=1),
-            conv34=ConvBNReLU(256, 256, 3, pad=1),
-            fc4=L.Linear(256 * 4 * 4, 1024, initialW=Uniform(1. / math.sqrt(256 * 4 * 4))),
-            fc5=L.Linear(1024, 1024, initialW=Uniform(1. / math.sqrt(1024))),
-            fc6=L.Linear(1024, n_classes, initialW=Uniform(1. / math.sqrt(1024)))
-        )
-        self.train = True
+    """11-layer VGG like Convolutional Neural Network."""
 
-    def __call__(self, x):
-        h = self.conv11(x, self.train)
-        h = self.conv12(h, self.train)
+    def __init__(self, n_classes, bc_learning=True, nobias=False, dr_ratio=0.5):
+        super(ConvNet, self).__init__()
+        self.dr_ratio = dr_ratio
+        self.bc_learning = bc_learning
+        if self.bc_learning:
+            self.loss = kl_divergence
+        else:
+            self.loss = F.softmax_cross_entropy
+        # architecture
+        kwargs = {'ksize': 3, 'stride': 1, 'pad': 1, 'nobias': nobias}
+        with self.init_scope():
+            self.conv1_1 = Conv2DBNActiv(3, 64, **kwargs)
+            self.conv1_2 = Conv2DBNActiv(64, 64, **kwargs)
+            self.conv2_1 = Conv2DBNActiv(64, 128, **kwargs)
+            self.conv2_2 = Conv2DBNActiv(128, 128, **kwargs)
+            self.conv3_1 = Conv2DBNActiv(128, 256, **kwargs)
+            self.conv3_2 = Conv2DBNActiv(256, 256, **kwargs)
+            self.conv3_3 = Conv2DBNActiv(256, 256, **kwargs)
+            self.conv3_4 = Conv2DBNActiv(256, 256, **kwargs)
+            self.fc4 = L.Linear(1024, initialW=Uniform(1. / math.sqrt(256 * 4 * 4)))
+            self.fc5 = L.Linear(1024, initialW=Uniform(1. / math.sqrt(1024)))
+            self.fc6 = L.Linear(n_classes, initialW=Uniform(1. / math.sqrt(1024)))
+
+    def __call__(self, x, t):
+        y_hat = self.forward(x)
+        loss = self.loss(y_hat, t)
+        if self.bc_learning:
+            t = F.argmax(t, axis=1)
+        acc = F.accuracy(y_hat, t)
+        chainer.report({'loss': loss, 'accuracy': acc}, self)
+        return loss
+
+    def forward(self, x):
+        # convolve input
+        h = self.conv1_2(self.conv1_1(x))
         h = F.max_pooling_2d(h, 2)
-
-        h = self.conv21(h, self.train)
-        h = self.conv22(h, self.train)
+        h = self.conv2_2(self.conv2_1(h))
         h = F.max_pooling_2d(h, 2)
-
-        h = self.conv31(h, self.train)
-        h = self.conv32(h, self.train)
-        h = self.conv33(h, self.train)
-        h = self.conv34(h, self.train)
-        h = F.max_pooling_2d(h, 2)
-
-        h = F.dropout(F.relu(self.fc4(h)), train=self.train)
-        h = F.dropout(F.relu(self.fc5(h)), train=self.train)
-
+        h = self.conv3_4(self.conv3_3(self.conv3_2(self.conv3_1(h))))
+        h = F.max_pooling_2d(h)
+        # linear
+        h = F.dropout(F.relu(self.fc4(h)), self.dr_ratio)
+        h = F.dropout(F.relu(self.fc5(h)), self.dr_ratio)
         return self.fc6(h)
